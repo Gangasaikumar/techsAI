26b61b73f2305ebb8da50539c88ddf8c54d7ec99
Gangasai kumar <gangasaikumar55555@gmail.com>
2026-02-07 23:23:28 +0530

    api url is changed

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create a centralized Axios instance
 const apiClient = axios.create({
-  baseURL: "https://techsai.in/api", // Backend URL
+  baseURL: "https://api.techsai.in", // Backend URL
   withCredentials: true, // Allow sending cookies if needed
 });
 
